7e9395bdba3a122df15422d58139b4a6a782f890
Jordan <jordansimons370@gmail.com>
2017-12-19 11:18:39 -0500

    Finishing Task 6

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,14 @@
 class PagesController < ApplicationController
+  require 'twilio-ruby'
   def home
   end
 
   def sms
+    @client = Twilio::REST::Client.new ENV['TWILIO_ACCOUNT_SID'], ENV['TWILIO_AUTH_TOKEN']
+
+    message = @client.messages.create(
+    body: params[:message],
+    to: params[:phone_number], # User Phone Number
+    from: ENV['TWILIO_FROM_NUMBER']) # Twilio Number
   end
 end
